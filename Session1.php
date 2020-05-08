2a6ec747f16833229c344fbd2f3f4d5a213b4f1a
fuckyou1907023 <ha9a1ltt@gmail.com>
2020-05-08 10:47:29 +0700

    first commit

---
+++ Session1.php
@@ -0,0 +1,15 @@
+<?php
+$x= 10; // la bien trong php
+$y=x+10;
+echo "y = ".$y;
+/*
+ * ko co kieu du lieu cu the
+ * number
+ * string
+ * array
+ * object
+ * boolean
+ */
+$x = "hello";
+$y = x+10; /* hello 10*/
+echo "y= ".$y;
